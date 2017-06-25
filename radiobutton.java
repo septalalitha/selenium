7ff503e2cefba9d4b4c880a7cd4d1444aec9acce
Lalitha <septa.lalitha@gmail.com>
2017-06-25 11:01:11 +0200

    removed unwanted comments
    removed unwanted comments

--- radiobutton.java
+++ radiobutton.java
@@ -5,16 +5,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class radiobutton {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.gecko.driver ","geckodriver");
-		
 		WebDriver driver=new FirefoxDriver();
-		
 		/*driver.get("https://book2.spicejet.com/");
 		driver.findElement(By.xpath("//input[@value='OneWay']")).click();
-		System.out.println(driver.findElements(By.xpath("//*[@name='ControlGroupSearchView$AvailabilitySearchInputSearchView$RadioButtonMarketStructure']")).size());
-	   */
-		
+				
 		driver.get("http://www.echoecho.com/htmlforms10.htm");
 		int count= driver.findElements(By.xpath("//input[@name='group1']")).size();
 		
