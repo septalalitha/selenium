5c9028bc39120eb9f9a666ccb09dc8d6e180ee29
Lalitha <septa.lalitha@gmail.com>
2017-06-25 11:02:54 +0200

    removed driver element
    removed driver element

--- radiobutton.java
+++ radiobutton.java
@@ -7,8 +7,6 @@ public class radiobutton {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver ","geckodriver");
 		WebDriver driver=new FirefoxDriver();
-		/*driver.get("https://book2.spicejet.com/");
-		driver.findElement(By.xpath("//input[@value='OneWay']")).click();
 				
 		driver.get("http://www.echoecho.com/htmlforms10.htm");
 		int count= driver.findElements(By.xpath("//input[@name='group1']")).size();
